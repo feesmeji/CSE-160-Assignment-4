bd396014db653f7ea58a15cda4a8444fbc38311e
feesmeji <153968214+feesmeji@users.noreply.github.com>
2024-05-22 14:59:50 -0700

    fixed sphere console errors

--- src/Sphere.js
+++ src/Sphere.js
@@ -24,16 +24,16 @@ class Sphere {
         // Pass the matrix to u_ModelMatrix attribute
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
         
-        var d = Math.PI/25;
-        var dd = Math.PI/25;
+        var d = Math.PI/10;
+        var dd = Math.PI/100;
 
         for (var t = 0; t < Math.PI; t += d) {
             for (var r = 0; r < (2*Math.PI); r += d) {
-                var p1 = [sin(t)*cos(r), sin(t)*sin(r), cos(t)];
+                var p1 = [Math.sin(t)*Math.cos(r), Math.sin(t)*Math.sin(r), Math.cos(t)];
                 
-                var p2 = [sin(t+dd)*cos(r), sin(t+dd)*sin(r), cos(t+dd)];
-                var p3 = [sin(t)*cos(r+dd), sin(t)*sin(r+dd), cos(t)];
-                var p4 = [sin(t+dd)*cos(r+dd), sin(t+dd)*sin(r+dd), cos(t+dd)];
+                var p2 = [Math.sin(t+dd)*Math.cos(r), Math.sin(t+dd)*Math.sin(r), Math.cos(t+dd)];
+                var p3 = [Math.sin(t)*Math.cos(r+dd), Math.sin(t)*Math.sin(r+dd), Math.cos(t)];
+                var p4 = [Math.sin(t+dd)*Math.cos(r+dd), Math.sin(t+dd)*Math.sin(r+dd), Math.cos(t+dd)];
 
                 var v = [];
                 var uv = [];
